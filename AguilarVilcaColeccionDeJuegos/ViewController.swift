9bec417acf8271a33514218ca47d59de2ddaec3b
Carlos123-A <carlos.aguilar.v@tecsup.edu.pe>
2024-05-06 16:44:02 -0500

    Initial Commit

---
+++ AguilarVilcaColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AguilarVilcaColeccionDeJuegos
+//
+//  Created by Carlos Aguilar on 6/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
